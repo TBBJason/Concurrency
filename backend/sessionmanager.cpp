fc1457a2941fbf1923ad4421184388833915ab3b
Jason Wu <wujason19@gmail.com>
2025-09-04 23:45:57 -0400

    starting new client

--- backend/sessionmanager.cpp
+++ backend/sessionmanager.cpp
@@ -1,21 +1,72 @@
 #include "sessionmanager.h"
+#include <iostream>
 
-
-void SessionManager::add(std::shared_ptr<websocket::stream<tcp::socket>> session) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    sessions_.insert(session);
-}    
-
-void SessionManager::remove(std::shared_ptr<websocket::stream<tcp::socket>> session) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    sessions_.erase(session);
+void SessionManager::add(ws_ptr ws) {
+    std::lock_guard<std::mutex> lk(mtx_);
+    sockets_.push_back(ws);
 }
 
-void SessionManager::broadcast(const std::string &message) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    for (auto& session : sessions_) {
-        session->write(net::buffer(message));
+void SessionManager::remove(ws_ptr ws) {
+    std::lock_guard<std::mutex> lk(mtx_);
+    sockets_.erase(std::remove(sockets_.begin(), sockets_.end(), ws), sockets_.end());
+    username_of_.erase(ws);
+    room_of_.erase(ws);
+}
+
+void SessionManager::set_username(ws_ptr ws, const std::string& username) {
+    std::lock_guard<std::mutex> lk(mtx_);
+    username_of_[ws] = username;
+}
+
+void SessionManager::set_room(ws_ptr ws, const std::string& room) {
+    std::lock_guard<std::mutex> lk(mtx_);
+    room_of_[ws] = room;
+}
+
+std::vector<std::string> SessionManager::list_users(const std::string& room) {
+    std::lock_guard<std::mutex> lk(mtx_);
+    std::vector<std::string> users;
+    for (auto &ws : sockets_) {
+        auto it = room_of_.find(ws);
+        if (it != room_of_.end() && it->second == room) {
+            auto un = username_of_.find(ws);
+            if (un != username_of_.end()) users.push_back(un->second);
+        }
+    }
+    return users;
+}
+
+void SessionManager::safe_send(ws_ptr ws, const json& message) {
+    try {
+        if (!ws) return;
+        ws->text(true);
+        auto s = message.dump();
+        ws->write(net::buffer(s));
+    } catch (const std::exception& e) {
+        std::cerr << "safe_spend error: " << e.what() << std::endl;
     }
 }
 
+void SessionManager::broadcast(const std::string &room, const json& message) {
+    std::lock_guard<std::mutex> lk(mtx_);
+    for (auto &ws : sockets_) {
+        auto it = room_of_.find(ws);
+        if (it != room_of_.end() && it->second == room) {
+            safe_send(ws, message);
+        }
+    }
+}
+
+
+bool SessionManager::send_to_user(const std::string& username, const json& message) {
+    std::lock_guard<std::mutex> lk(mutex_);
+    for (auto &ws : sockets_) {
+        auto un = username_of_.find(ws);
+        if (un != username_of_.end() && un->second == username) {
+            safe_send(ws, message);
+            return true;
+        }
+    }
+    return false;
+}
 
--- backend/sessionmanager.h
+++ backend/sessionmanager.h
@@ -15,6 +15,7 @@
 #include <thread>
 #include <set>
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 
 namespace beast = boost::beast;
@@ -23,16 +24,29 @@ namespace asio = boost::asio;
 namespace net = boost::asio;
 using tcp = asio::ip::tcp;
 
+using json = nlohmann::json;
+using ws_ptr = std::shared_ptr<websocket::stream<tcp::socket>>;
 
 class SessionManager {
 public:    
-    void add(std::shared_ptr<websocket::stream<tcp::socket>> session);
-    void remove(std::shared_ptr<websocket::stream<tcp::socket>> session);
-    void broadcast(const std::string& message);
+    void add(ws_ptr ws);
+    void remove(ws_ptr ws);
+
+    void set_username(ws_ptr ws, const std::string& username);
+    void set_room(ws_ptr, const std::string& room);
+
+    std::vector<std::string> list_users(const std::string& room);
+    void broadcast(const std::string& room, const json& message);
+    bool send_to_user(const std::string& username, const json& message);
 
 private:
-    std::set<std::shared_ptr<websocket::stream<tcp::socket>>> sessions_;
+    // std::set<std::shared_ptr<websocket::stream<tcp::socket>>> sessions_;
     std::mutex mutex_;
+    std::vector<ws_ptr> sockets_;
+    std::unordered_map<ws_ptr, std::string> username_of_;
+    std::unordered_map<ws_ptr, std::string> room_of_;
+
+    void safe_send(ws_ptr ws, const json& message);
 };
 
 #endif
